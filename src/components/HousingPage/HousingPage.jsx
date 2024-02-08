778956ac0506816074491cbc070b0eadc49885c5
Kailyan <kailyan.theresine@gmail.com>
2024-02-08 10:43:12 +0100

    Soutenance

--- src/components/HousingPage/HousingPage.jsx
+++ src/components/HousingPage/HousingPage.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import {NavLink, useParams } from 'react-router-dom';
+import {useParams } from 'react-router-dom';
 import housings from '../../API/logements.json';
 import Collapse from '../Layout/Collapse';
 import Gallery from '../Layout/Slides';
-import Host from '../Layout/Host';
 import RatingScale from './RatingScale';
 
 const HousingPage = () => {
 
     const { id } = useParams()
     const housing = housings.find(housing => housing.id === id)
-
-    if (housing === undefined) { 
-        return <section className="error_page">
-            <p className="error_page_subtitle">Malheureusement, le logement que vous recherchez n'est plus disponible ou n'existe pas.</p>
-            <NavLink title='Accueil' end to='/home' className="error_page_link">Retourner sur la page d'accueil</NavLink> 
-        </section>
-    }
-
 
     return (
         <section key={housing.id} className='house_page'>
@@ -37,8 +28,10 @@ const HousingPage = () => {
                 </article>
             
                 <article className='house_page_header_hoster'>
-                   <Host 
-                   />
+                <div className='house_page_header_hoster_host'>
+                <p className='house_page_header_hoster_host_name'>{housing.host.name}</p>
+                <img src={housing.host.picture} alt='host-cover' className='house_page_header_hoster_host_img'/>
+                </div>
                     <RatingScale
                         scaleValue={housing.rating}
                     />
